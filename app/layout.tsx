243b4f6a0d73a084e69aae0054fc16587b0f7193
Mickey Puri <mickeypuri@hotmail.com>
2024-02-11 05:49:55 +0000

    style layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from "react";
+import NavBar from "../components/NavBar";
 import "./globals.css";
-import Link from "next/link";
 
 interface LayoutProps {
   children: ReactNode;
@@ -9,27 +9,17 @@ interface LayoutProps {
 export default function RootLayout({ children } : LayoutProps) {
   return (
     <html lang="en">
-      <body className="px-">
+      <body className="bg-orange-50 flex flex-col min-h-screen px-4 py-2">
         <header>
-          <nav>
-            <ul>
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="/reviews">Reviews</Link>
-              </li>
-              <li>
-                <Link href="/about" prefetch={false}>About</Link>
-              </li>
-            </ul>
-          </nav>
+          <NavBar />
         </header>
-        <main>
+        <main className="grow py-3">
           {children}
         </main>
-        <footer>
-          Game data and images courtesy of <a href="https://rawg.io/" target="_blank">RAWG</a>
+        <footer className="text-center text-xs border-t py-3">
+          Game data and images courtesy of <a href="https://rawg.io/" target="_blank"
+          className="text-orange-800 hover:underline"
+          >RAWG</a>
         </footer>
       </body>
     </html>
