56ea6345b8337dd0bd4a3d3aec35d18b666a4816
Mickey Puri <mickeypuri@hotmail.com>
2024-02-11 20:37:31 +0000

    added markdown marked and graymatter

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({ children } : LayoutProps) {
         <main className="grow py-3">
           {children}
         </main>
-        <footer className="text-center text-xs border-t py-3">
+        <footer className="text-center text-xs border-t py-3 text-slate-500">
           Game data and images courtesy of <a href="https://rawg.io/" target="_blank"
           className="text-orange-800 hover:underline"
           >RAWG</a>
