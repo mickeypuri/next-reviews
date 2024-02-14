ef44c5da95fb2b056e26c319cc94e84363d80660
Mickey Puri <mickeypuri@hotmail.com>
2024-02-14 06:41:33 +0000

    client component

--- app/reviews/[slug]/page.tsx
+++ app/reviews/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import Heading from "@/components/Heading";
+import ShareLinkButton from "@/components/ShareLinkButton";
 import { getReview, getSlugs } from "@/lib/reviews";
 
 export async function generateStaticParams() {
@@ -17,7 +18,10 @@ export default async function ReviewPage({params : {slug}}) {
   return (
     <>
       <Heading>{title}</Heading>
-      <p className="italic pb-2">{date}</p>
+      <div className="flex gap-3 items-baseline">
+        <p className="italic pb-2">{date}</p>
+        <ShareLinkButton />
+      </div>
       <img src={image} alt="" 
         width="640" height="360"
         className="mb-2 rounded"
