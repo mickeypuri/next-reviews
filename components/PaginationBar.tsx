a90ad6defa53248c3715f6ad6c61fff9138adf0a
Mickey Puri <mickeypuri@hotmail.com>
2024-02-22 07:56:46 +0000

    pagination bar

--- components/PaginationBar.tsx
+++ components/PaginationBar.tsx
@@ -1,11 +1,33 @@
 import Link from "next/link";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
 
 export function PaginationBar({href, page, pageCount }) {
   return (
     <div className="flex gap-2 pb-3">
-      <Link href={`${href}?page=${page - 1}`}>&lt;</Link>
+      <PaginationLink href={`${href}?page=${page - 1}`} enabled={page > 1}>
+        <ChevronLeftIcon className="h-5 w-5" />
+        <span className="sr-only">Previous Page</span>
+      </PaginationLink>
       <span>Page {page} of {pageCount}</span>
-      <Link href={`${href}?page=${page + 1}`}>&gt;</Link>
+      <PaginationLink href={`${href}?page=${page + 1}`} enabled={page < pageCount}>
+        <ChevronRightIcon className="h-5 w-5" />
+        <span className="sr-only">Next Page</span>
+      </PaginationLink>
   </div>
   )
+}
+
+function PaginationLink({children, enabled, href}) {
+  if (!enabled) {
+    return (
+      <span className="border cursor-not-allowed rounded text-slate-300 text-sm">
+        {children}
+      </span>
+    );
+  }
+  return (
+    <Link href={href} className="border rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700">
+      {children}
+    </Link>
+  );
 }
