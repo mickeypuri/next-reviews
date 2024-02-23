b8c8aa11647ec3564d880991fa1c071b6b595425
Mickey Puri <mickeypuri@hotmail.com>
2024-02-23 07:08:43 +0000

    navlink usePathname

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,8 +1,18 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function NavLink({ href, children, prefetch = true }) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+  if (isActive) {
+    return (
+      <span className="text-orange-800 cursor-not-allowed">
+        {children}
+      </span>
+    )
+  }
   return (
     <Link href={href} 
       className="text-orange-800 hover:underline"
