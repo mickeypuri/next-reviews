1c174159b9ae968f71dee71095f81a5a946b1072
Mickey Puri <mickeypuri@hotmail.com>
2024-02-24 06:49:50 +0000

    searchbox working with dummy data

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { Combobox } from "@headlessui/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 // import { useIsClient } from "@/lib/hooks";
 
 const reviews = [
@@ -17,13 +19,28 @@ export default function SearchBox() {
     return null;
   } */
 
+  const router = useRouter();
+  const [query, setQuery] = useState("");
+  const filtered = reviews.filter((review) => review.title.includes(query));
+  const handleChange = (review) => {
+    //console.log("selected:", review);
+    router.push(`/reviews/${review.slug}`);
+  };
+
+  //console.log("[SearchBox] query:", query);
+  
   return (
     <div className="relative w-48">
-      <Combobox>
-        <Combobox.Input placeholder="Search..." className="border px-2 py-1 rounded w-full" />
+      <Combobox onChange={handleChange}>
+        <Combobox.Input 
+          value={query} 
+          placeholder="Search..."
+          onChange={(event) => setQuery(event.target.value)}
+          className="border px-2 py-1 rounded w-full"
+        />
         <Combobox.Options className="absolute bg-white py-1">
-          {reviews.map((review) => (
-            <Combobox.Option key={review.slug} value={review.slug}>
+          {filtered.map((review) => (
+            <Combobox.Option key={review.slug} value={review}>
               {({ active }) => (
                 <span className={`block px-2 truncate cursor-pointer ${active ? "bg-orange-100" : ""}`}>
                   {review.title}
