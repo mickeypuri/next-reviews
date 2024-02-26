08de349fc4667237d7ff2bf64a7e18c9bdf54bd2
Mickey Puri <mickeypuri@hotmail.com>
2024-02-26 07:44:07 +0000

    server action

--- components/CommentForm.tsx
+++ components/CommentForm.tsx
@@ -1,10 +1,25 @@
+import { createComment } from "@/lib/comments";
+
 export interface CommentFormProps {
   title: string;
+  slug: string;
 }
 
-export default function CommentForm({ title }: CommentFormProps) {
+export default function CommentForm({ title, slug }: CommentFormProps) {
+  async function action(formData: FormData) {
+    "use server";
+    console.log("[action] user:", formData.get("user"));
+    console.log("[action] message:", formData.get("message"));
+    const result = await createComment({
+      slug,
+      user: formData.get("user"),
+      message: formData.get("message")
+    });
+    console.log("created:", result);
+  }
+
   return (
-    <form className="border bg-white flex flex-col gap-2 mt-3 px-3 py-3 rounded">
+    <form action={action} className="border bg-white flex flex-col gap-2 mt-3 px-3 py-3 rounded">
       <p className="pb-1">
         Already played <strong>{title}</strong>? Have your say!
       </p>
@@ -12,13 +27,13 @@ export default function CommentForm({ title }: CommentFormProps) {
         <label htmlFor="userField" className="shrink-0 w-32">
           Your name
         </label>
-        <input id="userField" className="border px-2 py-1 rounded w-48" />
+        <input id="userField" name="user" className="border px-2 py-1 rounded w-48" />
       </div>
       <div className="flex">
-        <label htmlFor="messageField" className="shrink-0 w-32">
+        <label htmlFor="messageField"  className="shrink-0 w-32">
           Your comment
         </label>
-        <textarea id="messageField" className="border px-2 py-1 rounded w-full" />
+        <textarea id="messageField" name="message" className="border px-2 py-1 rounded w-full" />
       </div>
       <button type="submit"
         className="bg-orange-800 rounded px-2 py-1 self-center
